501d50ae50b2868a76175dd91fa38f075b9ca727
Michael Belyaev usrsse2 <usrsse2@icloud.com>
2016-05-08 15:31:00 +0300

    Added dependency to istanbul

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 node server.js & pid=$!
 sleep 2
-istanbul cover ./node_modules/mocha/bin/_mocha -- -R spec && cat ./coverage/lcov.info | ./node_modules/.bin/codecov
+./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha -- -R spec && cat ./coverage/lcov.info | ./node_modules/.bin/codecov
 #./node_modules/.bin/mocha
 OUT=$?
 kill -SIGINT $pid
