b5092f607c1aa224d37f139975318ab11813059d
Michael Belyaev usrsse2 <usrsse2@icloud.com>
2016-05-08 15:24:14 +0300

    Add codecov.io

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,8 @@
 
 node server.js & pid=$!
 sleep 2
-./node_modules/.bin/mocha
+istanbul cover ./node_modules/mocha/bin/_mocha -- -R spec && cat ./coverage/lcov.info | ./node_modules/.bin/codecov
+#./node_modules/.bin/mocha
 OUT=$?
 kill -SIGINT $pid
 exit $OUT
